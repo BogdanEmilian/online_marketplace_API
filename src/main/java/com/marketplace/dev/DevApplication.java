525a28be27854b7e684a2cf2e44378717ac4541d
BogdanEmilian <emilian.bogdan02@e-uvt.ro>
2023-10-02 22:48:58 +0300

    test

--- src/main/java/com/marketplace/dev/DevApplication.java
+++ src/main/java/com/marketplace/dev/DevApplication.java
@@ -3,6 +3,8 @@ package com.marketplace.dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TEST plus cleanup
+
 @SpringBootApplication
 public class DevApplication {
 
--- src/main/java/com/marketplace/dev/config/SecurityConfig.java
+++ src/main/java/com/marketplace/dev/config/SecurityConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import static org.springframework.security.config.Customizer.withDefaults;
